116e25d0c7da32f5220f38ec2861eba8873ee252
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-24 15:45:20 -0400

    tests added

---
+++ SongsApiIntegrationTests/BasicWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SongsApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongsApiIntegrationTests
+{
+    public class BasicWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+    }
+}
